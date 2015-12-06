f19dd9614330e08236b7c957677651e2b3bdbf6e
Lisa Murie <laiseramai@yahoo.com>
2015-12-07 02:29:23 +0300

    Add Node.py and LinkedList.py files

--- linkedlist/LinkedList.py
+++ linkedlist/LinkedList.py
@@ -1,1 +1,53 @@
 __author__ = 'lisa'
+from linkedlist.Node import Node
+
+class LinkedList(object):
+    def __init__(self):
+        self.head = None
+        self.counter = 0
+
+    def traverseList(self):
+
+        while self.head is not None:
+            print(" %d " % self.head.data)
+            self.head = self.head.nextNode
+
+    def size(self):
+        return self.counter
+
+    def insertStart(self, data):
+        self.counter += 1
+        newNode = Node(data)
+
+        if not self.head:
+            self.head = newNode
+
+        else:
+            newNode.nextNode = self.head
+            self.head = newNode
+
+    def insertEnd(self, data):
+        self.counter += 1
+        newNode = Node(data)
+        actualNode = self.head
+
+        if not self.head:
+            self.insertStart(data)
+            return
+
+        while actualNode.nextNode is not None:
+            actualNode = actualNode.nextNode
+
+        actualNode.nextNode = newNode
+
+    def remove(self, data):
+        self.counter -= 1
+        if self.head:
+            if self.head.data == data:
+                self.head = self.head.nextNode
+
+
+            else:
+                self.head.remove(data, self.head)
+
+
--- linkedlist/Node.py
+++ linkedlist/Node.py
@@ -1,1 +1,17 @@
 __author__ = 'lisa'
+
+class Node(object):
+    def __init__(self, data):
+        self.nextNode = None
+        self.data = data
+
+    def remove(self, data, previousNode):
+
+        if self.data == data:
+            previousNode.nextNode = self.nextNode
+            del self.data
+            del self.nextNode
+
+        else:
+            if self.nextNode is not None:
+                self.nextNode.remove(self, data)
